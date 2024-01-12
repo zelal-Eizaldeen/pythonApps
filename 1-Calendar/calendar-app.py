c705d7c9ade30b06efd780d28e7e24a494b048cb
Zelal EizAldeen <zelalezaldeen@gmail.com>
2024-01-12 09:18:14 +0300

    calendar app

---
+++ 1-Calendar/calendar-app.py
@@ -0,0 +1,7 @@
+import calender
+
+print('أهلا وسهلا بك ')
+year = int(input('الرجاء إدخال سنة مثلا ٢٠٢٤'))
+month = int(input('الرجاء إدخال رقم للشهر:'))
+print(calender.month(year, month))
+print('نتمني لك يوما سعيدا!')
